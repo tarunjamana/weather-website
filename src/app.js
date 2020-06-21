91cbc4359b5e14ace42fa11a197cbf46813c5526
tarun.jamana <tarunjamana@gmail.com>
2020-06-21 22:31:58 +0530

    changes done to the result text

--- src/app.js
+++ src/app.js
@@ -87,7 +87,8 @@ app.get('/weather',(req,res) =>{
           res.send({
             forecast:forecastData,
             location,
-            address:req.query.address
+            address:req.query.address,
+            
           })
         })
       } )
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,8 +10,8 @@ const forecast = (long,lat,callback) =>{
     } else if(body.error){
       callback('Unable to find location' ,undefined)
     } else {
-    
-      callback(undefined,body.current.weather_descriptions[0] + ` It is currently ${body.current.temperature} degrees out . it feels like ${body.current.feelslike} degrees out`)
+       
+      callback(undefined,body.current.weather_descriptions[0] + ` It is currently ${body.current.temperature} degrees out .The humidity is ${body.current.humidity} , it feels like ${body.current.feelslike} degrees out`)
     }
   })
 }
